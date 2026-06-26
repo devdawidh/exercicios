babfdfc9bf97fd06418bb7166ed5667fb67fadea
dawidh <davidsantiago3409@gmail.com>
2026-06-26 01:37:21 -0300

    Create for3.java

---
+++ for3.java
@@ -0,0 +1,29 @@
+//Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+//segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i<=x; i++) {
+            double y = sc.nextDouble();
+            double z = sc.nextDouble();
+
+            if (z == 0) {
+                System.out.println("Divisão impossivel");
+            } else {
+                double soma = y / z;
+                System.out.println(soma);
+            }
+        }
+        sc.close();
+    }
+
+}
