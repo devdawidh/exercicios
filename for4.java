203706014ec71947c994af042158e7156934e45e
dawidh <davidsantiago3409@gmail.com>
2026-06-26 23:05:25 -0300

    Create for4.java

---
+++ for4.java
@@ -0,0 +1,23 @@
+//Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+//começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+//exemplo.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            // o system vai por ordem numero, quadrado, cubo (porém a quebra vai pelo print)
+            System.out.print(i + " "); // n° da linha
+            System.out.print((i * i) + " "); //quadrado
+            System.out.println(i * i * i); //cubo
+            }
+
+        sc.close();
+        }
+    }
