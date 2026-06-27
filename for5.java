73d37d4c40f1438af09ed3d7fcd05d1ed5cc7d2e
dawidh <davidsantiago3409@gmail.com>
2026-06-27 02:21:25 -0300

    Update for5.java

--- for5.java
+++ for5.java
@@ -1,5 +1,3 @@
-pro gab:
-
 //Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
 //começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
 //exemplo.
